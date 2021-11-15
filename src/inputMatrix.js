16f8c1c85e52de596c341cc773701bbadbf3a2df
Sharath Cherian Thomas <sharathct22@gmail.com>
2021-11-15 21:49:07 +0000

    Some border and paddings

--- src/inputMatrix.js
+++ src/inputMatrix.js
@@ -2,13 +2,14 @@ var InputMatrix=(randomArray)=>{
     console.log(randomArray.randomArray);
     return(
         <div id="input">
-            <table>
+            <table style={{"borderWidth":"2px", 'borderColor':"#000000", 'borderStyle':'solid','margin':'20px'}}>
                 <tbody>
-                    {randomArray.randomArray.map(item=>{
+                    {randomArray.randomArray.map((item,index)=>{
                         return(
-                            <tr>
-                                {item.map((innerItem,index)=>{
-                                    return(<td key={index}>{innerItem}</td>)
+                            <tr key={index}>
+                                {item.map((innerItem,innerIndex)=>{
+                                    var string=index+"_"+innerIndex
+                                    return(<td key={string} style={{"borderWidth":"2px", 'borderColor':"#000000", 'borderStyle':'solid','padding':'10px','margin':'0px'}}>{innerItem}</td>)
                                 })}
                             </tr>
                         )
